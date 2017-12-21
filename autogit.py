d05298067216352274623e1743524580d69a22fa
fazejohk <johkmr@gmail.com>
2017-12-21 09:08:37 -0900

    done

--- autogit.py
+++ autogit.py
@@ -1,30 +1,3 @@
-#! /usr/bin/python3
-from tkinter import *
-from tkinter import ttk
 import os
 
-euid = os.geteuid()
-if euid != 0:
-    print("Script is not running as root please run it as root...")
-
-
-def Update(event):
-    os.system("sudo apt update; sudo apt upgrade -y")
-
-def Autogit(event):
-    commit=str(input("Commit:\n"))
-    os.system("git add .; git commit -m " + commit + "; git push pyto master")
-
-root=Tk()
-
-aub=Button(root, text="Autogit")
-aub.bind("<Button-1>", Autogit)
-aub.config(width=20, height=20)
-aub.pack(side=LEFT)
-
-upb=Button(root, text="Update")
-upb.bind("<Button-1>", Update)
-upb.config(width=20, height=20)
-upb.pack(side=RIGHT)
-
-root.mainloop()
+os.system("git add . && git commit -m done && git push pyto master")
--- keylogger/threadedclient.py
+++ keylogger/threadedclient.py
@@ -1,9 +1,105 @@
+# -*- coding: utf-8 -*-
 import time
 import socket
 import threading
+from passlib.hash import sha256_crypt
 
+#tee homma joka päivittää joka 5 minuutti ja näyttää kaikki jotka on onlines
 tLock = threading.Lock()
 shutdown = False
+
+def Admin():
+    global s
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((host, port))
+    s.setblocking(0)
+
+    global rT
+    rT = threading.Thread(target=receiving, args=("RecvThread", s))
+    rT.start()
+
+    alias = "Admin"
+    message = raw_input(alias + "-> ")
+    while message != 'q':
+        if message != '':
+            s.sendto(alias + ": " + message, server)
+            tLock.acquire()
+            message = raw_input(alias + "-> ")
+            tLock.release()
+            time.sleep(0.2)
+    if message == 'q':
+        s.sendto("FAGGOT" + alias + ": " + "Left ", server)
+        #tLock.acquire()
+        #tLock.release()
+def Osku():
+    global s
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((host, port))
+    s.setblocking(0)
+
+    global rT
+    rT = threading.Thread(target=receiving, args=("RecvThread", s))
+    rT.start()
+
+    alias = "Dr. MenBOB"
+    message = raw_input(alias + "-> ")
+    while message != 'q':
+        if message != '':
+            s.sendto(alias + ": " + message, server)
+            tLock.acquire()
+            message = raw_input(alias + "-> ")
+            tLock.release()
+            time.sleep(0.2)
+    if message == 'q':
+        s.sendto("FAGGOT" + alias + ": " + "Left ", server)
+        #tLock.acquire()
+        #tLock.release()
+def Vilske():
+    global s
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((host, port))
+    s.setblocking(0)
+
+    global rT
+    rT = threading.Thread(target=receiving, args=("RecvThread", s))
+    rT.start()
+
+    alias = "Vilske"
+    message = raw_input(alias + "-> ")
+    while message != 'q':
+        if message != '':
+            s.sendto(alias + ": " + message, server)
+            tLock.acquire()
+            message = raw_input(alias + "-> ")
+            tLock.release()
+            time.sleep(0.2)
+    if message == 'q':
+        s.sendto("FAGGOT" + alias + ": " + "Left ", server)
+        #tLock.acquire()
+        #tLock.release()
+def Tuukka():
+    global s
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((host, port))
+    s.setblocking(0)
+
+    global rT
+    rT = threading.Thread(target=receiving, args=("RecvThread", s))
+    rT.start()
+
+    alias = "Tuukka.Koistinen"
+    message = raw_input(alias + "-> ")
+    while message != 'q':
+        if message != '':
+            s.sendto(alias + ": " + message, server)
+            tLock.acquire()
+            message = raw_input(alias + "-> ")
+            tLock.release()
+            time.sleep(0.2)
+    if message == 'q':
+        s.sendto("FAGGOT" + alias + ": " + "Left ", server)
+        #tLock.acquire()
+        #tLock.release()
 
 def receiving(name, sock):
     while not shutdown:
@@ -17,25 +113,53 @@ def receiving(name, sock):
         finally:
             tLock.release()
 host = '192.168.10.36'
-port 12345
+port = 12345
 server = ('192.168.10.36', 12345)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.accept((host, port))
-s.setblocking(0)
+list = open("l.txt").readlines()
+hash = list[0].rstrip()
+hash1 = list[1].rstrip()
+hash2 = list[2].rstrip()
+hash3 = list[3].rstrip()
+print "Muista että sinun pitää päivittää viestit painamalla ENTER että näet onko tullut uusia viestejä\n"
+login = str(raw_input("LOGIN:\n"))
 
-rT = threading.Thread(target=receiving, args=("RecvThread", s))
-rT.start()
-
-alias = raw_input("Name: ")
-message = raw_input(alias + "-> ")
-while message != 'q':
-    if message != '':
-        s.sendto(alias + ": " + message, server)
-    tLock.acquire()
-    message = raw_input(alias + "-> ")
-    tLock.release()
-    time.sleep(0.2)
+if (sha256_crypt.verify(login, hash) == True):
+    print "Tervetuloa Vilske\n"
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((host, port))
+    s.setblocking(0)
+    s.sendto("Vilske" + " IS BACKK!!", server)
+    s.close()
+    Vilske()
+elif(sha256_crypt.verify(login, hash1) == True):
+    print "Tervetuloa Tuukka.Koistinen\n"
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((host, port))
+    s.setblocking(0)
+    s.sendto("Tuukka.Koistinen" + " IS BACKK!!", server)
+    s.close()
+    Tuukka()
+elif(sha256_crypt.verify(login, hash2) == True):
+    print "Tervetuloa DR. MenBOOOOOM\n"
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((host, port))
+    s.setblocking(0)
+    s.sendto("DR. MenBOOOOM" + " IS BACKK!!", server)
+    s.close()
+    Osku()
+elif(sha256_crypt.verify(login, hash3) == True):
+    print "Tervetuloa ADMIN\n"
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((host, port))
+    s.setblocking(0)
+    s.sendto("Admin" + " IS BACKK!!", server)
+    s.close()
+    Admin()
+else:
+    print "Salasana väärin :)"
+    time.sleep(1)
+    exit(0)
 
 shutdown = True
 rT.join()
