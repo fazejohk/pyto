a7fd2047c106a14eb1298e9c046d3eb52c7c95f1
johannes leppakorpi <johkmr@gmail.com>
2017-12-23 21:44:47 +0200

    done

--- OurChat/Beta/filetransfer.py
+++ OurChat/Beta/filetransfer.py
@@ -1,16 +1,18 @@
 import socket
 import sys
+import time
+
+name = time.strftime("%y-%m-%d-%H-%M-%S")
+
 s = socket.socket()
 s.bind(("192.168.10.36",9999))
 s.listen(10) # Acepta hasta 10 conexiones entrantes.
-
+print "Waiting for packets"
 while True:
     sc, address = s.accept()
 
     print address
-    i=1
-    f = open('file_'+ str(i)+".txt",'wb') #open in binary
-    i=i+1
+    f = open(str(name) + ".txt",'wb') #open in binary
     while (True):
     # recibimos y escribimos en el fichero
         l = sc.recv(1024)
